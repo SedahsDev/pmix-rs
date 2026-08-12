9cbeb5a3291e06afaebc1a984be5039a98f722e3
Matthew Baker <matt@matt-research.net>
2026-08-11 22:10:30 -0500

    fix(data_ops): use real ffi PMIX_STRING constant in double-free regression test

--- src/data_ops/tests.rs
+++ src/data_ops/tests.rs
@@ -2175,7 +2175,7 @@ use super::*;
             let data = libc::calloc(1, std::mem::size_of::<ffi::pmix_pdata_t>())
                 as *mut ffi::pmix_pdata_t;
             assert!(!data.is_null());
-            (*data).value.type_ = PMIX_STRING as _;
+            (*data).value.type_ = crate::ffi::PMIX_STRING as _;
             let string = std::ffi::CString::new("value").unwrap();
             (*data).value.data.string = libc::strdup(string.as_ptr());
             assert!(!(*data).value.data.string.is_null());
